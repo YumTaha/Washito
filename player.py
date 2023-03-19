4004669d03c17827b44e03c0e467d5a867701c07
YumTaha <69929602+YumTaha@users.noreply.github.com>
2023-03-19 01:16:54 -0400

    added graphics

--- player.py
+++ player.py
@@ -1,30 +1,42 @@
 import pygame
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
+    def __init__(self, pos):
         super().__init__()
-        self.image = pygame.Surface((size, size))
+        self.image = pygame.Surface((32, 64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft= pos)
-        
-        self.speed = 5
-        self.gravity = 0
-        self.initial_rect_y = self.rect.y
 
-    def movement(self):
+        # player movement
+        self.direction = pygame.math.Vector2()
+        self.speed = 6
+        self.gravity = 0.8
+        self.jump_speed = -16
+
+    def import_character_assets(self):
+        character_path = 'graphics/character/'
+        self.animations = {'idle':[], 'run':[], 'jump':[], 'fall':[]}
+
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+
+    def get_input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_RIGHT]: self.rect.x += 1 *self.speed
-        elif keys[pygame.K_LEFT]: self.rect.x -= 1 *self.speed
-        if keys[pygame.K_SPACE] or keys[pygame.K_UP]: self.gravity = -20
+        if keys[pygame.K_RIGHT]: self.direction.x = 1 
+        elif keys[pygame.K_LEFT]: self.direction.x = -1 
+        else: self.direction.x = 0
+
+        if keys[pygame.K_SPACE] or keys[pygame.K_UP]: self.jump()
 
     def apply_gravity(self):
-        if self.rect.y < self.initial_rect_y + 20: self.gravity += 1
-        self.rect.y += self.gravity
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
 
+    def jump(self):
+        self.direction.y = self.jump_speed
 
-
-    def update(self, x_shift):
-        self.apply_gravity()
-        self.movement()
-        self.rect.x += x_shift
+    def update(self):
+        self.get_input()
